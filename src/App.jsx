6f7d7db42b1c2e905cb5be7e29f5d3df4a9f2dff
Chazz Gagliano <chazzgagliano@Chazzs-MacBook-Air.local>
2023-07-05 21:10:49 -0400

    create new activity form

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,35 @@
+import { useState } from "react"
+
+
 export default function App() {
-  return "hi"
+    useState()
+  return (
+    <>
+      <form className="form-item-new">
+      <div className="form-row">
+        <label htmlFor="item">New Activity</label>
+        <input type="text" id="item"  />
+      </div>
+      <button className="btn">Add</button>
+      </form>
+      <h1 className="header">To Do List</h1>
+      <ul className="list">
+        <li>
+            <label>
+                <input type="checkbox" />
+               Task 1
+            </label>
+            <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+            <label>
+                <input type="checkbox" />
+               Task 2
+            </label>
+            <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  )
 }
+
